5c9b3aae06bb93b8ce3b20a709e57622ca9d288e
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-05-25 02:06:24 +0500

    Сделал модельное окно для тех поддержки

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ToggleTheme } from '@components/ToggleTheme';
+import { Link } from 'expo-router';
+import { useNavigation } from '@react-navigation/native';
+import HelpScreen from 'app/home/HelpModal';
 
 const Stack = createStackNavigator();
 
-// Создаем навигационный стек
 function HomeScreen() {
   return (
     <View style={styles.container}>
@@ -15,11 +17,24 @@ function HomeScreen() {
 }
 
 export default function HomeStack() {
+  const navigation = useNavigation();
+
+  const openModal = () => {
+    navigation.navigate('Modal')
+  }
+
+
   return (
-    <Stack.Navigator>
+    <Stack.Navigator >
       <Stack.Screen name="index" component={HomeScreen} options={{
-        headerTitle: 'Главный экран' 
+        headerTitle: 'Главный экран',
+         headerRight: () => (
+          <Pressable style={{width: '85%'}} onPress={() => navigation.navigate('Modal')}>
+            <Text>Поддержка</Text>
+          </Pressable>
+        ),
       }} />
+      <Stack.Screen  name='Modal' component={HelpScreen} options={{presentation: 'modal'}} />
     </Stack.Navigator>
   );
 }
